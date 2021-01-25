e26b07a9b361d055ac718622ff802b48c0962471
sahasrag <71852686+sahasrag@users.noreply.github.com>
2021-01-25 17:41:45 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var database,player, form, game, playerCount;
+var gameState = 0;
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+   
+}
+
